1e238cfacbf56ebdb1bac06e9b87763686a853aa
tl4k <techlab4kids.apps@gmail.com>
2024-04-15 18:18:59 +0200

    fixes module adds mqtt extensions (should be fixed)

--- npm_link.sh
+++ npm_link.sh
@@ -5,6 +5,14 @@ cd ../tl4k-ide-render
 npm link
 
 cd ../tl4k-ide-gui
+
+npm link tl4k-ide-render tl4k-ide-vm
+mv node_modules/scratch-vm node_modules/scratch-vm.old
+mv node_modules/tl4k-ide-vm node_modules/scratch-vm
+
+mv node_modules/scratch-render node_modules/scratch-render.old
+mv node_modules/tl4k-ide-render node_modules/scratch-render
+
 npm link tl4k-ide-render tl4k-ide-vm
 
 # webpack-dev-server
